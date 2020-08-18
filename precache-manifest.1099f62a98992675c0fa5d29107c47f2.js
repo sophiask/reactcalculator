84d9dc12c2a11cac7bccfa688a688335180d2bfc
ashu <sophiashakya1@hotmail.com>
2020-08-17 22:02:24 -0500

    Updates

---
+++ precache-manifest.1099f62a98992675c0fa5d29107c47f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85dc33eda7990c7d7378e2cf1a8757a",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "0679da1ba8c8993e3ecb",
+    "url": "/reactcalculator/static/css/main.71c47740.chunk.css"
+  },
+  {
+    "revision": "441be31b22556701fccd",
+    "url": "/reactcalculator/static/js/2.c56d5072.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/reactcalculator/static/js/2.c56d5072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0679da1ba8c8993e3ecb",
+    "url": "/reactcalculator/static/js/main.72ca104b.chunk.js"
+  },
+  {
+    "revision": "597c20d2772d9b324c9e",
+    "url": "/reactcalculator/static/js/runtime-main.710c946f.js"
+  }
+]);
